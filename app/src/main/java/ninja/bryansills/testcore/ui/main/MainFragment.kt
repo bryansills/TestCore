0f9da8d6428524be92fe0dbff40fdb69eff424af
Bryan Sills <bsills@bignerdranch.com>
2019-08-19 14:58:19 -0500

    initial commit

---
+++ app/src/main/java/ninja/bryansills/testcore/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package ninja.bryansills.testcore.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import ninja.bryansills.testcore.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    var text = "Hello"
+        set(message) {
+            activity?.findViewById<TextView>(R.id.message)?.text = message
+            field = message
+        }
+}
